7f44a8db5c3538417043709f3b4a0ec5da02efe4
Savchenko <savchenko.ra@yandex.ru>
2023-02-08 13:53:07 +0300

    Solution of the first problem.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte peas = 120;
+        short chery = 32023;
+        int apple = 100500;
+        long coconut = 9008007006005004000l;
+        double size = 7.00062;
+        float temp = 36.6f;
+        // Объявляем переменные и инициализируем их.
+        System.out.println("Значение переменной peas c типом byte равно: " + peas);
+        System.out.println("Значение переменной chery c типом short равно: " + chery);
+        System.out.println("Значение переменной apple c типом byte равно: " + apple);
+        System.out.println("Значение переменной coconut c типом byte равно: " + coconut);
+        System.out.println("Значение переменной size c типом double равно: " + size);
+        System.out.println("Значение переменной temp c типом float равно: " + temp);
+        // Выводим переменные и их значения в консоль.
     }
 }
